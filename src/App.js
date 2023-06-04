4495f04ad83125400d05b94fe357f2215dcde6f4
Pickle-cpu <xiongyu0124@gmail.com>
2023-06-04 00:33:47 -0400

    v4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// v3
+// v4
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V4</h1>
       </header>
     </div>
   );
